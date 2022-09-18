966a8dc9b06fc3496bb3fde5b7f87d38a4a4ad52
Valentine <mselinkor@gmail.com>
2022-09-18 20:54:03 +0300

    fix

--- src/app/homework2-item/homework2-item.component.ts
+++ src/app/homework2-item/homework2-item.component.ts
@@ -1,18 +1,24 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-homework2-item',
   templateUrl: './homework2-item.component.html',
   styleUrls: ['./homework2-item.component.scss']
 })
-export class Homework2ItemComponent implements OnInit {
+export class Homework2ItemComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-
+  ngAfterViewInit() {
+    if(this.additionalContent)
+    {
+      this.additionalContent.nativeElement.style.fontSize = "25px";
+    }
+  }
+  @ContentChild('additionalContent') additionalContent : ElementRef;
   @Output() onButtonClick = new EventEmitter();
   @Input() imgSrc = 'https://cdn-icons-png.flaticon.com/512/27/27482.png';
   @Input() title : string = ' Title';
